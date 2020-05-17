e03302cf2222c0ce4d7584ff5a205949f1f24964
JBI <22871153+jbilandzija@users.noreply.github.com>
2020-05-17 20:05:24 +0200

    Initial commit

---
+++ src/main/kotlin/de/novatec/studentservice/StudentserviceApplication.kt
@@ -0,0 +1,11 @@
+package de.novatec.studentservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentserviceApplication>(*args)
+}
